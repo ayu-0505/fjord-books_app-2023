e89993e0549ad89efb3cd59a4ab0e958aecf7b8a
ayu-0505 <amehuri.boshi.sh@icloud.com>
2023-09-25 13:38:51 +0900

    訳文上で変数を利用するように変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,7 +25,7 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to book_url(@book), notice: t('.created') }
+        format.html { redirect_to book_url(@book), notice: t('.created', name: Book.model_name.human) }
         format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class BooksController < ApplicationController
   def update
     respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to book_url(@book), notice: t('.updated') }
+        format.html { redirect_to book_url(@book), notice: t('.updated', name: Book.model_name.human) }
         format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class BooksController < ApplicationController
     @book.destroy
 
     respond_to do |format|
-      format.html { redirect_to books_url, notice: t('.destroyed') }
+      format.html { redirect_to books_url, notice: t('.destroyed', name: Book.model_name.human) }
       format.json { head :no_content }
     end
   end
