49dee222354b3140fe8ee1a13810a0ab81ac7153
ayu-0505 <amehuri.boshi.sh@icloud.com>
2023-10-23 09:54:19 +0900

    新規属性のストロングパラメーター追加、ログアウトエラー解消

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,17 @@ class UsersController < ApplicationController
   end
 
   def show
+    # @user = User.find(params[:id])
+    set_user
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :post_number, :address, :self_introduction)
   end
 end
