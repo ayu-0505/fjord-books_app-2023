55e207dd5a1ec58a50a5d29ce72af1c62fd6bf55
ayu-0505 <amehuri.boshi.sh@icloud.com>
2023-11-24 11:22:02 +0900

    仮想親ごとのコメントコントローラー作成、本と日報のviewのshowにコメント表示とコメント入力欄追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,75 +1,24 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
-  before_action :set_report
-  before_action :set_comment, only: %i[show edit update destroy]
-
-  # GET /comments or /comments.json
-  def index
-    @comments = @report.comments
-  end
-
-  # GET /comments/1 or /comments/1.json
-  def show
-  end
-
-  # GET /comments/new
-  def new
-    @comment = @report.comments.build
-  end
-
-  # GET /comments/1/edit
-  def edit
-  end
-
-  # POST /comments or /comments.json
   def create
-    @comment = @report.comments.build(comment_params)
-
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to [@report, @comment], notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
+    @comment = @commentable.comments.build(comment_params)
+    @comment.user = current_user
+    @comment.save
+    redirect_to [@commentable], notice: 'Your comment was successfully posted.'
   end
 
-  # PATCH/PUT /comments/1 or /comments/1.json
-  def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to [@report, @comment], notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /comments/1 or /comments/1.json
   def destroy
-    @comment.destroy
-
-    respond_to do |format|
-      format.html { redirect_to [@report], notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
+    @comment = @commentable.comments.find(params[:id])
+    if @comment.user == current_user
+      @comment.destroy
+      redirect_to [@commentable], notice: 'Comment was successfully destroyed.'
+    else
+      render @commentable
     end
   end
 
   private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_comment
-    @comment = Comment.find(params[:id])
-  end
-
-  def set_report
-    @report = Report.find(params[:report_id])
-  end
 
   # Only allow a list of trusted parameters through.
   def comment_params
