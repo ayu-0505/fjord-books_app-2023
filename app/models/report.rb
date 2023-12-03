2640dccb2f59c37f03bcc9abc4b7050c66dc589f
ayu-0505 <amehuri.boshi.sh@icloud.com>
2023-12-03 22:08:13 +0900

    BookモデルとUserモデルのポリモーフィックコード共通化を解除

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Report < ApplicationRecord
-  include Commentable
   belongs_to :user
   validates :title, presence: true
   validates :content, presence: true
+  has_many :comments, as: :commentable, dependent: :destroy
 end
