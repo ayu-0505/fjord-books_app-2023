8715db359eb04c4924e593e8d11674dbc447a58f
ayu-0505 <amehuri.boshi.sh@icloud.com>
2023-12-04 09:49:25 +0900

    インスタンス変数をローカル変数に変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,16 +2,16 @@
 
 class CommentsController < ApplicationController
   def create
-    @comment = @commentable.comments.build(comment_params)
-    @comment.user = current_user
-    @comment.save
-    redirect_to [@commentable], notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    comment = @commentable.comments.build(comment_params)
+    comment.user = current_user
+    comment.save
+    redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
   end
 
   def destroy
-    @comment = current_user.comments.find(params[:id])
-    @comment.destroy
-    redirect_to [@commentable], notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+    comment = current_user.comments.find(params[:id])
+    comment.destroy
+    redirect_to @commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
   end
 
   private
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -23,9 +23,9 @@ class ReportsController < ApplicationController
 
   # POST /reports or /reports.json
   def create
-    @report = current_user.reports.new(report_params)
-    if @report.save
-      redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human)
+    report = current_user.reports.new(report_params)
+    if report.save
+      redirect_to report_url(report), notice: t('controllers.common.notice_create', name: Report.model_name.human)
     else
       render :new, status: :unprocessable_entity
     end
