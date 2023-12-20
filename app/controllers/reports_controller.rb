b73c51c758cf0646c6dfbe78deff16f7f88bb116
ayu-0505 <amehuri.boshi.sh@icloud.com>
2023-12-20 11:41:04 +0900

    コントローラーの処理をReportモデルに移動、localhost:3000をpathに追加

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -22,7 +22,7 @@ class ReportsController < ApplicationController
     @report = current_user.reports.new(report_params)
 
     if @report.save
-      update_mentions(mentioning_id_list)
+      @report.update_mentions(@report.mentioning_id_list)
       redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
     else
       render :new, status: :unprocessable_entity
@@ -31,7 +31,7 @@ class ReportsController < ApplicationController
 
   def update
     if @report.update(report_params)
-      update_mentions(mentioning_id_list)
+      @report.update_mentions(@report.mentioning_id_list)
       redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
       render :edit, status: :unprocessable_entity
@@ -40,7 +40,7 @@ class ReportsController < ApplicationController
 
   def destroy
     ActiveRecord::Base.transaction do
-      delete_mentions(mentioning_id_list)
+      @report.delete_mentions(@report.mentioning_id_list)
       @report.destroy
     end
 
@@ -55,38 +55,5 @@ class ReportsController < ApplicationController
 
   def report_params
     params.require(:report).permit(:title, :content)
-  end
-
-  def update_mentions(id_list)
-    old_id_list = @report.mentioning_relations.map(&:mentioned_id)
-    list_to_create = id_list.difference(old_id_list)
-    list_to_delete = old_id_list.difference(id_list)
-
-    create_mentions(list_to_create) unless list_to_create.empty?
-    delete_mentions(list_to_delete) unless list_to_delete.empty?
-  end
-
-  def mentioning_id_list
-    paths = URI.extract(@report.content, %w[http https])
-    return [] if paths == []
-
-    paths.select { |path| path.match?(('127.0.0.1:3000' && %r{reports/(\d+)})) }.map do |path|
-      match = path.match(%r{reports/(\d+)})
-      match[1].to_i
-    end.uniq
-  end
-
-  def create_mentions(id_list)
-    id_list.each do |id|
-      mention = @report.mentioning_relations.build(mentioned_id: id)
-      mention.save unless id == @report.id
-    end
-  end
-
-  def delete_mentions(id_list)
-    id_list.each do |id|
-      mention = @report.mentioning_relations.find_by(mentioned_id: id)
-      mention.delete
-    end
   end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -28,4 +28,37 @@ class Report < ApplicationRecord
   def created_on
     created_at.to_date
   end
+
+  def update_mentions(id_list)
+    old_id_list = mentioning_relations.map(&:mentioned_id)
+    list_to_create = id_list.difference(old_id_list)
+    list_to_delete = old_id_list.difference(id_list)
+
+    create_mentions(list_to_create) unless list_to_create.empty?
+    delete_mentions(list_to_delete) unless list_to_delete.empty?
+  end
+
+  def mentioning_id_list
+    paths = URI.extract(content, %w[http https])
+    return [] if paths == []
+
+    paths.select { |path| path.match?((('127.0.0.1:3000' || 'localhost:3000') && %r{reports/(\d+)})) }.map do |path|
+      match = path.match(%r{reports/(\d+)})
+      match[1].to_i
+    end.uniq
+  end
+
+  def create_mentions(id_list)
+    id_list.each do |id|
+      mention = mentioning_relations.build(mentioned_id: id)
+      mention.save unless id == self.id
+    end
+  end
+
+  def delete_mentions(id_list)
+    id_list.each do |id|
+      mention = mentioning_relations.find_by(mentioned_id: id)
+      mention.delete
+    end
+  end
 end
