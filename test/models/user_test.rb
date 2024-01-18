f0761953d9f7d7e531803e1b08823ca3bf7e36d8
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-01-18 14:43:08 +0900

    不要なコントローラーテストを削除、Userモデルの単体テストを作成

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'name_or_email with user name' do
+    user = users(:alice)
+    assert_equal 'alice', user.name_or_email
+  end
+
+  test 'name_or_email without user name' do
+    user = users(:alice)
+    user.name = ''
+    assert_equal 'alice@example.com', user.name_or_email
+  end
 end
