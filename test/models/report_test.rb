7d59c92ccbaf3624347f6f27781548281fbc95b9
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-01-19 14:47:45 +0900

    Fixtureの作成、user単体テストを修正、report単体テストを作成

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,69 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:alice)
+    @report = reports(:alice_report)
+  end
+  test 'editable? returns true with correct user' do
+    assert @report.editable?(@user)
+  end
+
+  test 'editable? returns false with incorrect user' do
+    incorrect_user = users(:bob)
+    assert_not @report.editable?(incorrect_user)
+  end
+
+  test 'created_on returns Date class' do
+    @report.created_at = Time.zone.now
+    assert_instance_of Date, @report.created_on
+  end
+
+  test 'create new mentions' do
+    existing_report1 = reports(:bob_report)
+    existing_report2 = reports(:carol_report)
+    report = @user.reports.new(
+      title: 'おすすめ日報',
+      content: "http://localhost:3000/reports/#{existing_report1.id}と
+                http://localhost:3000/reports/#{existing_report2.id}がおすすめです。"
+    )
+    report.save
+
+    assert_equal 2, report.active_mentions.size
+    assert report.active_mentions.find_by(mentioned_by_id: existing_report1.id)
+    assert report.active_mentions.find_by(mentioned_by_id: existing_report2.id)
+  end
+
+  test 'delete exsinting mentions and create new mentions when content updates' do
+    mentioned_report = reports(:bob_report)
+
+    # Fixtureの確認(既存Mentionがある状態)
+    assert_equal 1, @report.active_mentions.size
+    assert @report.active_mentions.find_by(mentioned_by_id: mentioned_report)
+
+    new_mentioned_report = reports(:carol_report)
+    @report.content =
+      "http://localhost:3000/reports/#{new_mentioned_report.id}を見ました。"
+    @report.save
+
+    assert_equal 1, @report.active_mentions.size
+    assert @report.active_mentions.find_by(mentioned_by_id: new_mentioned_report.id)
+    assert_not @report.active_mentions.find_by(mentioned_by_id: mentioned_report.id)
+  end
+
+  test 'report_mentions should be uniq' do
+    mentioned_report = reports(:bob_report)
+    @report.content = "http://localhost:3000/reports/#{mentioned_report.id}がおすすめ。
+                      http://localhost:3000/reports/#{mentioned_report.id}だよ。"
+    @report.save
+
+    assert_equal 1, @report.active_mentions.size
+  end
+
+  test 'report shold have no mention for self' do
+    @report.content = "http://localhost:3000/reports/#{@report.id}がこの日報のURLです。"
+    @report.save
+
+    assert_equal 0, @report.active_mentions.size
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,14 +3,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test 'name_or_email with user name' do
-    user = users(:alice)
-    assert_equal 'alice', user.name_or_email
+  setup do
+    @user = users(:alice)
   end
 
-  test 'name_or_email without user name' do
-    user = users(:alice)
-    user.name = ''
-    assert_equal 'alice@example.com', user.name_or_email
+  test 'name_or_email returns user name with user name' do
+    assert_equal 'alice', @user.name_or_email
+  end
+
+  test 'name_or_email returns email without user name' do
+    @user.name = ''
+    assert_equal 'alice@example.com', @user.name_or_email
   end
 end
