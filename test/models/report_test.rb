8d781d855160e3a7de08498b71e22ca5e3aa59e0
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-01-19 15:24:34 +0900

    reportの単体テストを修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -6,6 +6,7 @@ class ReportTest < ActiveSupport::TestCase
   setup do
     @user = users(:alice)
     @report = reports(:alice_report)
+    @mentioned_report = reports(:bob_report)
   end
   test 'editable? returns true with correct user' do
     assert @report.editable?(@user)
@@ -21,45 +22,24 @@ class ReportTest < ActiveSupport::TestCase
     assert_instance_of Date, @report.created_on
   end
 
-  test 'create new mentions' do
-    existing_report1 = reports(:bob_report)
-    existing_report2 = reports(:carol_report)
-    report = @user.reports.new(
-      title: 'おすすめ日報',
-      content: "http://localhost:3000/reports/#{existing_report1.id}と
-                http://localhost:3000/reports/#{existing_report2.id}がおすすめです。"
-    )
-    report.save
-
-    assert_equal 2, report.active_mentions.size
-    assert report.active_mentions.find_by(mentioned_by_id: existing_report1.id)
-    assert report.active_mentions.find_by(mentioned_by_id: existing_report2.id)
-  end
-
-  test 'delete exsinting mentions and create new mentions when content updates' do
-    mentioned_report = reports(:bob_report)
-
-    # Fixtureの確認(既存Mentionがある状態)
-    assert_equal 1, @report.active_mentions.size
-    assert @report.active_mentions.find_by(mentioned_by_id: mentioned_report)
-
+  test 'delete existing mentions and create new mentions when content updates' do
     new_mentioned_report = reports(:carol_report)
     @report.content =
       "http://localhost:3000/reports/#{new_mentioned_report.id}を見ました。"
     @report.save
 
     assert_equal 1, @report.active_mentions.size
+    assert_not @report.active_mentions.find_by(mentioned_by_id: @mentioned_report.id)
     assert @report.active_mentions.find_by(mentioned_by_id: new_mentioned_report.id)
-    assert_not @report.active_mentions.find_by(mentioned_by_id: mentioned_report.id)
   end
 
   test 'report_mentions should be uniq' do
-    mentioned_report = reports(:bob_report)
-    @report.content = "http://localhost:3000/reports/#{mentioned_report.id}がおすすめ。
-                      http://localhost:3000/reports/#{mentioned_report.id}だよ。"
+    @report.content = "http://localhost:3000/reports/#{@mentioned_report.id}がおすすめ。
+                      http://localhost:3000/reports/#{@mentioned_report.id}だよ。"
     @report.save
 
     assert_equal 1, @report.active_mentions.size
+    assert @report.active_mentions.find_by(mentioned_by_id: @mentioned_report.id)
   end
 
   test 'report shold have no mention for self' do
