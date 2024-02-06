71106371b8ee69b9453a17997b61310d2ec98b43
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-02-06 09:48:53 +0900

    reportモデルの単体テストを修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -8,6 +8,7 @@ class ReportTest < ActiveSupport::TestCase
     @report = reports(:alice_report)
     @mentioned_report = reports(:bob_report)
   end
+
   test 'editable?は@userが日報作成者ならtrueを返す' do
     assert @report.editable?(@user)
   end
@@ -17,29 +18,24 @@ class ReportTest < ActiveSupport::TestCase
     assert_not @report.editable?(incorrect_user)
   end
 
-  test 'created_onはDateクラスを返す' do
+  test 'created_onはDateクラスかつ日報の作成された日付を返す' do
     @report.created_at = Time.zone.now
-    assert_instance_of Date, @report.created_on
+    assert_equal Date.today, @report.created_on # rubocop:disable Date cop
   end
 
   test '日報の保存時は既存の言及を全削除し、新規の言及を作成する' do
-    assert_equal 1, @report.active_mentions.size
-    assert @report.active_mentions.find_by(mentioned_by_id: @mentioned_report.id)
-
+    assert_equal [@mentioned_report], @report.mentioning_reports
     new_mentioned_report = reports(:carol_report)
     @report.content =
       "http://localhost:3000/reports/#{new_mentioned_report.id}を見ました。"
-    @report.save
-
-    assert_equal 1, @report.active_mentions.size
-    assert_not @report.active_mentions.find_by(mentioned_by_id: @mentioned_report.id)
-    assert @report.active_mentions.find_by(mentioned_by_id: new_mentioned_report.id)
+    @report.save!
+    assert_equal [new_mentioned_report], @report.reload.mentioning_reports
   end
 
   test '同じ言及先の言及は作成されない' do
     @report.content = "http://localhost:3000/reports/#{@mentioned_report.id}がおすすめ。
                       http://localhost:3000/reports/#{@mentioned_report.id}だよ。"
-    @report.save
+    @report.save!
 
     assert_equal 1, @report.active_mentions.size
     assert @report.active_mentions.find_by(mentioned_by_id: @mentioned_report.id)
@@ -47,15 +43,14 @@ class ReportTest < ActiveSupport::TestCase
 
   test '自身への言及は作成されない' do
     @report.content = "http://localhost:3000/reports/#{@report.id}がこの日報のURLです。"
-    @report.save
+    @report.save!
 
-    assert_equal 0, @report.active_mentions.size
+    assert_empty @report.active_mentions
   end
 
   test '存在しない日報のID番号のURLを言及しても言及は作成されない' do
-    num = reports.size
-    @report.content = "http://localhost:3000/reports/#{@report.id + rand(num..num + 100)}は存在しない日報だ。"
-    @report.save
+    @report.content = "http://localhost:3000/reports/#{- @report.id}は存在しない日報だ。"
+    @report.save!
 
     assert_equal 0, @report.active_mentions.size
   end
